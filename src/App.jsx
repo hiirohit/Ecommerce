1e218033fa729dd179a34bfaa3950a7d8c4a8f4c
rohit <kumarrohit41175@gmail.com>
2026-02-20 15:52:32 +0530

    product card make

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Products from './components/Products'
 
 function App() {
   return (
-    <div>App
-      
-    </div>
+    <>
+    <Products />
+    </>
   )
 }
 
---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,90 @@
+import { FaShoppingCart } from 'react-icons/fa'
+import React, { useState } from 'react'
+import ProductViewModal from "./ProductViewModal"
+function ProductCard({
+    productId,
+      productName,
+      image,
+      description,
+      quantity,
+      price,
+      discount,
+      specialPrice,
+}) 
+    {
+    
+    const [openProductViewModal, setOpenProductViewModal] = useState(false);
+    const btnLoader = false;
+    const [selectedViewProdeuct, setSelectedViewProduct] = useState("");
+    const isAvailable = quantity && Number(quantity) > 0;
+
+    const handleProductView = (product) => {
+        setSelectedViewProduct(product);
+        setOpenProductViewModal(true);
+    }
+
+  return (
+    <div className='border rounded-lg shrink-x1 overflow-hidden transition-shadow duration-300'>
+        <div onClick={() => {handleProductView({
+                id : productId,
+                productName,
+                image,
+                description,
+                quantity,
+                price,
+                discount,
+                specialPrice,
+        })}} className='w-full overflow-hidden aspect-[3/2]'>
+            <img src= {image} alt={productName} className='w-full h-full cursor-pointer transition-transform duration-300 transform hover:scale-120'/>
+        </div>
+        <div className='p-4'>
+            <h2 onClick={() => {handleProductView({
+                    id: productId,
+                    productName,
+                    image,
+                    description,
+                    quantity,
+                    price,
+                    discount,
+                    specialPrice,
+            })}} className='text-lg font-semibold mb-2 cursor-pointer'>{productName}</h2>
+            <div className='min-h-20 max-h-20'>
+                <p className='text-gray-600 text-sm'>{description}</p>
+            </div>
+            <div className='flex items-center justify-between'>
+                {specialPrice ? (
+                <div className='flex flex-col'>
+                    <span className='text-gray-400 line-through'>
+                        ${Number(price).toFixed(2)} 
+                    </span>
+                    <span className='text-xl font-bold text-gray-700'>
+                        ${Number(specialPrice).toFixed(2)} 
+                    </span>
+                </div>
+                ) : (
+                    <span className='text-xl font-bold text-gray-700'>
+                        {" "}
+                        ${Number(price).toFixed(2)} 
+                    </span>
+                )}
+                <button 
+                    disabled={!isAvailable || btnLoader}
+                    onClick={() => {}} 
+                    className={`bg-blue-500 ${isAvailable ? "opacity-100 hover:bg-blue-600" : "opacity-70"}
+                        text-white py-2 px-3 rounded-lg items-center transition-color duration-300 w-36 flex justify-center`}>
+                    <FaShoppingCart className="mr-2"/>
+                    {isAvailable ? "Add to cart" : "Stock Out"}
+                </button>
+            </div>
+        </div>
+        <ProductViewModal 
+            open={openProductViewModal}
+            setOpen={setOpenProductViewModal}
+            product={selectedViewProdeuct}
+            isAvailable={isAvailable}
+        />
+    </div>
+  )
+}
+
+export default ProductCard
